0d5a07fed7373e492d72e2fd9a19a3ca5b23bee1
Sneha Yadav <lionessy8@gmail.com>
2026-05-05 22:58:04 +0530

    day3

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-var allElems = document.querySelectorAll('.elem')
-var allFullElems = document.querySelectorAll('.fullElem')
+function openFeatures() {
+    var allElems = document.querySelectorAll('.elem')
+    var fullElemPage = document.querySelectorAll('.fullElem')
+    var fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
 
-allElems.forEach(function(elem) {
-    elem.addEventListener('click', function() {
-        var index = parseInt(elem.id);
-        allFullElems[index].style.display = 'block'
+    allElems.forEach(function(elem) {
+        elem.addEventListener('click', function() {
+            var index = parseInt(elem.id);
+            fullElemPage[index].style.display = 'block'
+        })
     })
+
+    fullElemPageBackBtn.forEach(function(back) {
+        back.addEventListener('click', function() {
+            fullElemPage[back.id].style.display = 'none'
+        })
+    })
+}
+
+// openFeatures();
+let form = document.querySelector('.addTask form')
+let taskInput = document.querySelector('.addTask  form input')
+let taskDetailsInput = document.querySelector('.addTask  form textarea')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    console.log(taskInput.value);
+    console.log(taskDetailsInput.value);
 })
