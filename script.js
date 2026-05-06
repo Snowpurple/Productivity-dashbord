d460a3ab77730a1d646c7bd36992d754dc0e37c1
Sneha Yadav <lionessy8@gmail.com>
2026-05-06 16:18:33 +0530

    day4

--- script.js
+++ script.js
@@ -17,14 +17,54 @@ function openFeatures() {
     })
 }
 
-// openFeatures();
+openFeatures();
 let form = document.querySelector('.addTask form')
 let taskInput = document.querySelector('.addTask  form input')
 let taskDetailsInput = document.querySelector('.addTask  form textarea')
+
+let taskCheckBox = document.querySelector('.addTask form #check')
+
+
+let currentTask = [{
+        task: 'Mandir jao',
+        details: 'Har din mandir jao',
+        imp: true
+    },
+    {
+        task: 'Recording karo',
+        details: 'cohort ki',
+        imp: true
+    }, {
+        task: 'Lunch at 2pm',
+        details: 'nhi to mammi marengi',
+        imp: false
+    },
+]
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 
     console.log(taskInput.value);
     console.log(taskDetailsInput.value);
+    console.log(taskCheckBox.checked);
 })
+
+function renderTask() {
+    var allTask = document.querySelector('.allTask')
+
+    var sum = ''
+
+    currentTask.forEach(function(elem) {
+        sum += `
+        <div class="task">
+            <h5>${elem.task}
+            <span class="${elem.imp ? 'imp' : ''}"></span>
+            </h5>
+            <button>Completed</button>
+        </div>`
+
+    })
+    allTask.innerHTML = sum
+}
+
+renderTask();
